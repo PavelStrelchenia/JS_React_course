779d412875c1b838979c171819e950689b61c837
Pavel Strelchenia <pivlik5608@gmail.com>
2026-04-16 19:38:08 +0300

    Added a folder from lesson 12

---
+++ 12/js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Вам есть 18 лет?", "18");
+
+// const answer = +prompt("Вам есть 18 лет?", "18"); + - Number
+
+// console.log(typeof answer); // string
+
+// Вся инфа от пользователя приходит string
+
+const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как вашеа фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+console.log(typeof answers); // object
+console.log(typeof null); // object
