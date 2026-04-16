287e11d4816b10a5bb998222182a0075752b9c93
Pavel Strelchenia <pivlik5608@gmail.com>
2026-04-16 19:37:02 +0300

    Added a folder from lesson 10

---
+++ 10_dataTypes/js/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// Number
+let number = 4;
+
+console.log(4 / 0); // Infinity
+
+console.log(-4 / 0); // -Infinity
+
+console.log("string" * 9); // NaN
+
+// String
+
+const person = "Alex";
+
+// Boolean
+
+const boolTrue = true;
+const boolFalse = false;
+
+// Null
+
+// console.log(somthing); // ReferenceError
+
+// Undefined
+
+let und;
+
+console.log(und); // Undefined
+
+// Symbol
+
+// BigInt
+
+// Object
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false,
+};
+
+console.log(obj.name);
+console.log(obj["age"]);
+
+// Array (object)
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp", 6, {}, []];
+
+console.log(arr[2]);
