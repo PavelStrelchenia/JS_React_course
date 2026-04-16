6c6f9267c178957d6a600a719096952a824262a3
Pavel Strelchenia <pivlik5608@gmail.com>
2026-04-16 19:39:37 +0300

    Added a folder from lesson 20

---
+++ 20/js/script.js
@@ -0,0 +1,100 @@
+"use strict";
+
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log("Я сыт");
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "dwdwddwd");
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log("Все сыты");
+// } else {
+//     console.log("Мы уходим");
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola === 2) || (fries === 3 && nuggets)) {
+//     console.log("Все довольны");
+// } else {
+//     console.log("Мы уходим");
+// }
+
+// let johnReport,
+//     alexReport,
+//     samReport,
+//     mariaReport = "done";
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log(!0);
+
+// TASK
+
+// console.log(NaN || 2 || undefined); // 2 +
+
+// console.log(NaN && 2 && undefined); // NaN +
+
+// console.log(1 && 2 && 3); // 3 +
+
+// console.log((!1 && 2) || !3); // false +
+
+// console.log(25 || (null && !3)); // 25 +
+
+// console.log(NaN || null || !3 || undefined || 5); // 5 +
+
+// console.log(NaN || (null && !3 && undefined) || 5); // 5 +
+
+// console.log((5 === 5 && 3 > 1) || 5); // true
+
+// TASK
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//     console.log("Done!");
+// }
+
+// // "Done!"
+
+// TASK
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//     console.log("Done!");
+// }
+
+// // "Done!"
+
+//TASK
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if ((hamburger && cola) || (fries === 3 && nuggets)) {
+    console.log("Done!");
+}
+// No
