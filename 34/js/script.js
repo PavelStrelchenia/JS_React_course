0e6147c366c4e87755ca2a071a262905849278fd
Pavel Strelchenia <pivlik5608@gmail.com>
2026-04-17 16:12:35 +0300

    Added a folder from lesson 34

---
+++ 34/js/script.js
@@ -0,0 +1,99 @@
+"use strict";
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1,
+};
+
+// const copy = obj; // Ссылка
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4,
+    },
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20,
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+
+const oldArray = ["a", "b", "c"];
+
+const newArray = oldArray.slice();
+
+newArray[1] = "ffwfwwf";
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ["youtube", "vimeo", "rutube"],
+    blogs = ["wp", "LJ", "bg"],
+    internet = [...video, ...blogs, "vk", "fb"];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newArr = [...array];
+
+console.log(newArr);
+
+const objjj = {
+    one: 1,
+    two: 2,
+};
+
+const newObj = { ...objjj };
