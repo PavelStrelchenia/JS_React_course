29bb1ebe3c0933d7d756a88eca23bb4183754ecd
Pavel Strelchenia <pivlik5608@gmail.com>
2026-04-18 09:48:10 +0300

    Added a folder from task 11

---
+++ task_11/script/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const family = ["Peter", "Ann", "Alex", "Linda"];
+
+function showFamily(arr) {
+    if (arr.length === 0) {
+        return "Семья пуста";
+    } else {
+        return `Семья состоит из: ${arr.join(" ")}`;
+    }
+}
+
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings(arr) {
+    arr.forEach((city) => {
+        console.log(city.toLowerCase());
+    });
+}
